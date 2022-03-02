b5ff18bd3596220a8420c5500a60a2e7bc56b360
“hagleyson” <hagleyson@gmail.com>
2022-03-02 12:34:26 -0300

    fix: toast

--- App.tsx
+++ App.tsx
@@ -5,20 +5,66 @@ import { Provider } from "react-redux";
 import AppNavigator from "./src/navigation";
 import {store} from "./src/store"
 
-import Toast from 'react-native-toast-message';
+import Toast,{BaseToast,ErrorToast} from 'react-native-toast-message';
 
 if(Platform.OS === 'android') { 
   require('intl'); 
   require('intl/locale-data/jsonp/pt-BR'); 
 }
 
+const toastConfig ={
+  success:(props:any)=>(
+    <BaseToast
+    {...props}
+    style={{height:100,borderColor:"#00ff2a"}}
+    text1Style={{
+      fontSize:18,
+      fontWeight:"400"
+    }}
+    text2Style={{
+      fontSize:16,
+      fontWeight:"400"
+    }}
+
+    />
+  ),
+  error:(props:any)=>(
+    <ErrorToast 
+    {...props}
+    style={{height:100,borderColor:"#ff0000"}}
+    text1Style={{
+      fontSize:18,
+      fontWeight:"400"
+    }}
+    text2Style={{
+      fontSize:16,
+      fontWeight:"400"
+    }}
+    />
+  ),
+  info:(props:any)=>(
+    <ErrorToast 
+    {...props}
+    style={{height:100,borderColor:"#48a4f0"}}
+    text1Style={{
+      fontSize:18,
+      fontWeight:"400"
+    }}
+    text2Style={{
+      fontSize:16,
+      fontWeight:"400"
+    }}
+    />
+  )
+
+}
 
 
 export default function App() {
   return (
     <Provider store={store}>      
       <AppNavigator />
-      <Toast />
+      <Toast config={toastConfig} />
     </Provider>
   );
 }
